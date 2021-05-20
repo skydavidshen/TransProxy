5f87efb3d552763737f04bdb5c5a3e8c07a10ac5
davidshen <david.shen@heavengifts.com>
2021-05-20 12:33:53 +0800

    getProxyIp

--- service/trans-platform/ipidea.go
+++ service/trans-platform/ipidea.go
@@ -2,6 +2,7 @@ package trans_platform
 
 import (
 	"TransProxy/lib/proxyip"
+	"TransProxy/model/business"
 	"TransProxy/utils"
 	url2 "net/url"
 )
@@ -11,8 +12,13 @@ const url = "http://tiqu.linksocket.com:81/abroad?num=1&type=2&lb=1&sb=0&flow=1&
 type IpIdea struct {}
 
 func (i *IpIdea) ProxyUrl() *url2.URL {
-	proxyIps := proxyip.NewIpIdea(url).GetProxy()
-	proxyIp := proxyIps[0]
+	proxyIp := getProxyIp()
 	urlProxy := utils.BuildIpIdeaUrl(proxyIp.IP, proxyIp.Port)
 	return urlProxy
+}
+
+// 获取proxy IP，可以通过其他形式获取(并不一定需要实时通过网络URL请求)，例如：从IP池缓存，hashmap本地缓存等
+func getProxyIp() business.ProxyIP {
+	proxyIps := proxyip.NewIpIdea(url).GetProxy()
+	return proxyIps[0]
 }
